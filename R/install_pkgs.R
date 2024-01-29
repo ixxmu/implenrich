671661afbf48317fa83de015c5024ef267756741
ixxmu <jjliulangjj@gmail.com>
2024-01-29 15:48:27 +0800

    chinese_font

--- R/install_pkgs.R
+++ R/install_pkgs.R
@@ -32,6 +32,7 @@
 #' @importFrom magrittr %>%
 #' @importFrom pak pkg_install
 #' @importFrom utils install.packages
+#' @importFrom showtext showtext_auto
 
 install_packages_with_pak <- function(packages) {
   # 检查pak是否已安装
@@ -42,6 +43,19 @@ install_packages_with_pak <- function(packages) {
   # 使用pak安装所需包
   pak::pkg_install(packages)
 }
+
+# 还需要解决字体的问题，我的服务器不会显示中文
+load_font_ch<-function(yes=1){
+  if (!requireNamespace("showtext", quietly = TRUE)) {
+    pak::pkg_install("showtext")
+  }
+  require("showtext")
+  showtextdb::font_install(source_han_serif())
+  showtext::showtext_auto(enable = TRUE)
+}
+
+
+# load_font_ch(1)
 
 # 使用示例
 # install_packages_with_pak(pkgs_lists)
